9be602665103eade33627de623e7fa4319321ecf
Jerson Celestin <jersoncelestin@Jersons-MacBook-Air.local>
2023-06-24 19:05:41 -0400

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const About = () => {
+  return <div name="about" className="w-full h-screen bg-gradient-to-b
+  from-gray-800 to-black text-white"
+  >
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full">
+            <div className="pb-8 ">
+                <p className="text-4xl font-bold inline border-b-4
+                border-gray-500">
+                About
+                </p>
+            </div>
+
+            <p className="text-xl mt-20">
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+               In quas, quae quod repudiandae laborum deserunt eum id, 
+               molestias explicabo minus sequi! Molestias, obcaecati! 
+               Deleniti vero earum libero nesciunt molestias adipisci 
+               optio maxime ratione praesentium ullam aut explicabo totam 
+               harum, necessitatibus voluptatum, id sed fuga. Ad earum assumenda 
+               ullam nam maxime. 
+            </p>
+
+            <br />
+
+            <p className="text-xl">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                 Cum commodi repellat velit, iure repudiandae nisi quas est 
+                 explicabo alias deleniti nulla voluptatum inventore non ducimus. 
+                 Maiores neque laboriosam aperiam incidunt consequatur quam. Ipsa 
+                 assumenda libero iure, illum laborum soluta ducimus? Totam architecto 
+                 praesentium, temporibus adipisci magnam consectetur fugit recusandae id?
+            </p>
+        </div>
+
+    </div>;
+}
+
+export default About
